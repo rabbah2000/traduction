6e4333e0b24fe71342bf0c43cd766283a95d545e
rabbah2000 <wwiimranerabb197611@gmail.com>
2025-08-05 15:53:58 +0100

    chnage

--- main.py
+++ main.py
@@ -1,36 +1,27 @@
-from fastapi import FastAPI , UploadFile, File
+from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import StreamingResponse
-import requests
-import json
-import sys 
 import io
-# ...existing code...
+import google.generativeai as genai
+
 app = FastAPI()
 
-@app.get("/github_user") 
-def get_github_user():
- url = "https://api.github.com/users/octocat"
- response= requests.get(url)
- return (response.json())
-
+genai.configure(api_key="AIzaSyAp2G0EQDjvzdazNTT78qyceLSFZRUmGJM")
+model = genai.GenerativeModel("gemini-2.5-flash")
 @app.get("/")
-def read_root():
-    return {"message": "Bienvenue sur mon API FastAPI"}
+def get_root():
+    return {"message": "Bienvenue dans l'API de traduction du darija marocain vers le français."}
 @app.post("/translate/")
 async def translate_file(file: UploadFile = File(...)):
     contents = await file.read()
     lignes = contents.decode().splitlines()
 
-    output = io.StringIO()  # Pour construire le fichier texte en mémoire
+    output = io.StringIO()
 
-    for line in lignes:
-        if line.strip() :  # Ignore les lignes vides
-            traduction = line.strip()
-            output.write(traduction + "\n")
-        else:
-            output.write("\n")  # Garde les lignes vides
-
-    output.seek(0)  # Revenir au début du fichier pour l’envoi
+    
+    prompt = f"Traduire ce texte du darija marocain vers le français et c'est un time code tu le laisse comment il est  n'ajoute rien de ta téte juste traduit le texte qu on il s'agit d une ligne texte ne me dit pas aucune chose  :\n{li}"
+    response = model.generate_content(prompt)
+    traduction = response.text.strip()
+    output.write(traduction + "\n")
 
     return StreamingResponse(output, media_type="text/plain", headers={
         "Content-Disposition": f"attachment; filename=traduit.txt"
